02529b9c19e169638bda587010255d32231ea1c9
anjalikagitha <kagithanagaanjali@gmail.com>
2024-09-30 12:28:39 +0530

    Create Area.c

---
+++ Area.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int a,b,c;
+    float s,area;
+    printf("Enter the values of a ,b,c:");
+    scanf("%d%d%d",&a,&b,&c);
+    s=(a+b+c)/2;
+    area=sqrt(s*(s-a)*(s-b)*(s-c));
+    printf("The area of triangle is %f",area);
+    
+    
+}
