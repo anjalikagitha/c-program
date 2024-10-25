a1b09d841f87a834367eb6e026ff6d4633b16874
anjalikagitha <kagithanagaanjali@gmail.com>
2024-10-25 12:14:36 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,16 @@
+//even or odd number
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter n :");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("n is even");
+    }
+   else
+   {
+    printf("n is odd");
+   }
+}
