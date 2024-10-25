02805c2bbe136ccf7871ac3e1db27f1af9efc793
anjalikagitha <kagithanagaanjali@gmail.com>
2024-10-25 11:57:47 +0530

    perimeter.c

---
+++ perimeter.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int l,b;
+    float p;
+    printf("enter a length and breadth of a rectangle:");
+    scanf("%d%d",&l,&b);
+    p =(2*(l+b));
+    printf("the perimeter of rectangle is %f", p);
+    
+}
