aff3b5c63404a4a13d375979350045f7bf69b8ed
anjalikagitha <kagithanagaanjali@gmail.com>
2024-10-30 15:02:01 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+void main()
+{
+    char ch;
+    printf("enter a character : ");
+    scanf("%c",&ch);
+    switch(ch)
+    {
+        case'a':
+        printf("it is a vowel");
+        break;
+        case'e':
+        printf("it is a vowel");
+        break;
+        case'i':
+        printf("it is a vowel");
+        break;
+        case'o':
+        printf("it is a vowel");
+        break;
+        case'u':
+        printf("it is a vowel");
+        break;
+        case'A':
+        printf("it is a vowel");
+        break;
+        case'E':
+        printf("it is a vowel");
+        break;
+        case'I':
+        printf("it is a vowel");
+        break;
+        case'O':
+        printf("it is a vowel");
+        break;
+        case'U':
+        printf("it is a vowel");
+        break;
+        default:
+        printf("not a vowel");
+        break;
+    }
+}
