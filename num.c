7e7fa2d7b835d318aa1a2d3c945da325a9ae1be9
anjalikagitha <kagithanagaanjali@gmail.com>
2024-12-20 12:06:22 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    
+    int i,n;
+    printf("enter number:\n");
+    scanf("%d",&n);
+    printf("the values from 1 to n is:\n");
+    for(i=1;i<=n;i++)
+    {
+        
+        printf("%d\n",i);
+    
+    }
+    return 0;
+}
